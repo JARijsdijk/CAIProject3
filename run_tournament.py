1be1718893d7cdb5883e712e5e5bcf452967d5aa
Thomas Baars <T.C.Baars@student.tudelft.nl>
2023-04-04 13:08:10 +0200

    Start of tournament skeleton

--- run_tournament.py
+++ run_tournament.py
@@ -18,8 +18,8 @@ if not RESULTS_DIR.exists():
 tournament_settings = {
     "agents": [
         {
-            "class": "agents.template_agent.template_agent.TemplateAgent",
-            "parameters": {"storage_dir": "agent_storage/TemplateAgent"},
+            "class": "agents.agent61.agent61.Agent61",
+            "parameters": {"storage_dir": "agent_storage/Agent61"},
         },
         {
             "class": "agents.boulware_agent.boulware_agent.BoulwareAgent",
